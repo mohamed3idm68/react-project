736d4300acc44c396177ec10fbc20d8cd5c20b22
mohamed eid <85568105+mohamed3idm68@users.noreply.github.com>
2024-06-29 02:07:23 +0300

    navbar update

--- src/componants/Navbar/Navbar.js
+++ src/componants/Navbar/Navbar.js
@@ -1,17 +1,40 @@
-
 import { Link } from "react-router-dom";
-
-
+import "./Navbar.css";
 
 
 
 function Navbar() {
-    return (
-      <div className="Navbar">
-          <Link to="/">Home</Link>
-          <Link to="/Contact">Contact</Link>
+  return (
+    <div className="Navbar">
+      <div className="container">
+        <div className="logo">
+          <h2 className="logo-text">Ultra Profile</h2>
+        </div>
+        <ul className="ul-list">
+          <li className="list-item">
+            <Link exact to="/">
+              Home
+            </Link>
+          </li>
+          <li className="list-item">
+            <a href="#">Work</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Portfolio</a>
+          </li>
+          <li className="list-item">
+            <a href="#">Resume</a>
+          </li>
+          <li className="list-item">
+            <a href="#">About</a>
+          </li>
+          <li className="list-item">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
       </div>
-    );
-  }
-  
-  export default Navbar;
+    </div>
+  );
+}
+
+export default Navbar;
