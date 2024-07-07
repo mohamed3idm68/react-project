f43d11b18aa3c6163fde918745690d88d6c0c75e
mohamed eid <85568105+mohamed3idm68@users.noreply.github.com>
2024-07-07 22:20:36 +0300

    my work section

--- src/componants/Work/Work.js
+++ src/componants/Work/Work.js
@@ -1,12 +1,46 @@
+import "./Work.css"
 
 
 
 function Work() {
-    return (
-      <div className="Work">
-         Work
+  return (
+    <div className="work">
+      <div className="container">
+        <h2 className="work-title">
+          <span>My</span> Work
+        </h2>
+        <div className="part first">
+          <i className="icon fa fa-chain fa-2x"></i>
+          <h4 className="part-title">Mobile Ux</h4>
+          <hr className="line" />
+          <p className="part-desc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+            quos omnis voluptas ea ipsam! Voluptas.
+          </p>
+        </div>
+
+        <div className="part">
+          <i className="icon fa fa-bolt fa-2x"></i>
+          <h4 className="part-title">Mobile Ux</h4>
+          <hr className="line" />
+          <p className="part-desc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+            quos omnis voluptas ea ipsam! Voluptas.
+          </p>
+        </div>
+
+        <div className="part last">
+          <i className="icon fa fa-tachometer fa-2x"></i>
+          <h4 className="part-title">Mobile Ux</h4>
+          <hr className="line" />
+          <p className="part-desc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+            quos omnis voluptas ea ipsam! Voluptas.
+          </p>
+        </div>
       </div>
-    );
-  }
-  
-  export default Work;
+    </div>
+  );
+}
+
+export default Work;
