9a063de6fd6dd7e2e84bde056098bd7dbe1a2fbd
mohamed eid <85568105+mohamed3idm68@users.noreply.github.com>
2024-08-08 04:34:36 +0300

    social media secion

--- src/componants/About/About.js
+++ src/componants/About/About.js
@@ -1,11 +1,31 @@
-
+import "./About.css";
 
 function About() {
-    return (
-      <div className="About">
-         About
+  return (
+    <>
+      <div className="creative">
+        <div className="container">
+          <div className="creative-info">
+            <h2 className="info-title">
+              <span>This is</span> Me
+            </h2>
+            <h4 className="info-dir">Creative Director</h4>
+            <p className="info-desc">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est{" "}
+              <a href="#">explicabo</a> maxime quibusdam quaerat nihil aliquid
+              aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad
+              veritatis?
+            </p>
+            <p className="info-desc">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est
+              explicabo maxime quibusdam quaerat nihil aliquid aperiam deleniti,
+              repellendus dolorum. Possimus nisi cupiditate ad veritatis?
+            </p>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default About;
+    </>
+  );
+}
+
+export default About;
--- src/componants/Social media/SocialMedia.js
+++ src/componants/Social media/SocialMedia.js
@@ -1,16 +1,37 @@
-
-
-
-
-
-
+import { useEffect, useState } from "react";
+import "./social.css";
+import axios from "axios";
 
 function SocialMedia() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("../js/data.json").then((res) => {
+      setData(res.data.social);
+    });
+  }, []);
+
+  const myData = data.map((i) => {
     return (
-      <div className="SocialMedia">
-         SocialMedia
-      </div>
+      <>
+        <div className="social face twitter pin" >
+          <i className={i.icon}></i>
+          <p>
+            <span className="info1">{i.title}</span>
+            <span className="info2">{i.body}</span>
+          </p>
+        </div>
+      </>
     );
-  }
-  
-  export default SocialMedia;
+  });
+
+  return (
+    <>
+      <div className="social-media">
+         {myData}
+      </div>
+    </>
+  );
+}
+
+export default SocialMedia;
